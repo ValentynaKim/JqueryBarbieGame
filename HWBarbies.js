6af87b9c14a1835b3868186d4a66d1529e65fac0
Valentyna Kim <tinakim2105@gmail.com>
2019-07-16 18:13:58 -0700

    adding completed js

--- HWBarbies.js
+++ HWBarbies.js
@@ -1,4 +1,4 @@
-var Dolls=[
+var Dolls = [
     {
         name: "MiniWan",
         hp: 100,
@@ -23,12 +23,12 @@ var Dolls=[
         rp: 19,
         counter: 11
     },
-    
+
 ]
 var par1;
 var par2;
 
-$(".firstImage").on("click",function(){
+$(".firstImage").on("click", function () {
     if ($(".yourCharacter").children().length <= 1) {
         $(".firstImage").appendTo(".DollsToRun");
         $(this).appendTo(".yourCharacter");
@@ -38,8 +38,8 @@ $(".firstImage").on("click",function(){
         par1RP = $(par1).attr("rp");
         par1RP = parseInt(par1RP);
         par1Counter = $(par1).attr("counter");
-        par1Counter =  parseInt(par1Counter);
-    } else if ($(".defender").children().length <= 1){
+        par1Counter = parseInt(par1Counter);
+    } else if ($(".defender").children().length <= 1) {
         $(this).appendTo(".defender");
         par2 = $(this);
         par2HP = $(par2).attr("hp");
@@ -47,7 +47,7 @@ $(".firstImage").on("click",function(){
         par2RP = $(par2).attr("rp");
         par2RP = parseInt(par2RP);
         par2Counter = $(par2).attr("counter");
-        par2Counter =  parseInt(par2Counter);
+        par2Counter = parseInt(par2Counter);
     } else {
         console.log("you have choose your character");
     }
@@ -55,14 +55,45 @@ $(".firstImage").on("click",function(){
 
 })
 
-$("button").on("click", function() {
+$("button").on("click", function () {
     if (($(".yourCharacter").children().length > 1) && ($(".defender").children().length > 1)) {
         par2HP -= par1RP;
         $(".defender p:last-child").text("HP: " + par2HP);
         par1RP += par1Counter;
         par1HP -= par2RP;
         $(".yourCharacter p:last-child").text("HP: " + par1HP);
-
+        win();
+        lose();
     }
 
-}) 
+});
+
+
+function win() {
+    if (par2HP <= 0) {
+        $(par2).remove();
+
+    };
+
+}
+
+function lose() {
+    if (par1HP <= 0) {
+        $(par1).remove();
+        $("button").text("Restart");
+        $("button").on("click", function () {
+            document.location.reload();
+        })
+    }
+}
+
+function winAll() {
+    if (($(".defender").children().length = 1) && ($(".DollsToRun").children().length = 1)) {
+        $(".fightSection").text("You won!");
+        $("button").text("Restart");
+        $("button").on("click", function () {
+            document.location.reload();
+        })
+}
+}
+
